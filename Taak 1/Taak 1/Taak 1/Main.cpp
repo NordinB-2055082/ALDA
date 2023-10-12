e6d543d261c37be11350c53a78e72712d773ea0a
unknown <nordin.ben-al-lal@student.uhasselt.be>
2023-10-12 20:20:08 +0200

    opmaak project

---
+++ Taak 1/Taak 1/Taak 1/Main.cpp
@@ -0,0 +1,14 @@
+// Taak 1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    //variant 1
+
+
+
+
+}
+
---
+++ Taak 1/Taak 1/Taak 1/Variant_1.cpp
@@ -0,0 +1,11 @@
+#include "Variant_1.h"
+#include <iostream>
+
+
+// 1. Query by full name
+//  hashtable:
+//	key = volledige naam 
+//	value = contact object
+//
+// 2. Delete by full name:
+//	gebruik dezelfde opzoekmanier om daarna te verwijderen 
